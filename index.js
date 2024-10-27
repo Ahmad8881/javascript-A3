e7c1871207753d57059dce10b10c1270276d8650
Ahmad8881 <1221mohammadahmad@gmail.com>
2024-10-27 15:59:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+
+// ans Number 1
+
+
+// rows = 5
+
+// let pattern = '';
+
+// for (let i = 0; i <= rows; i++) {
+//     for (let j = 0; j <= i; j++) {
+
+//         pattern += "* "
+//     }
+//     pattern += "\n"
+// }
+// console.log(pattern)
+
+
+//   ans Number 2
+
+
+// let count = 0;
+// let count1 = 0;
+
+// for (let i = 0; i <= 45; i++) {
+
+//     if (i % 3 === 0) {
+
+//         console.log("fizz")
+
+//     } else if (i % 5 === 0) {
+
+//         console.log("buzz")
+//     } else if (i % 3 === 0 && i % 5 === 0) {
+
+//         console.log("fizzbuzz")
+
+//     } else {
+
+//         console.log(i)
+//     }
+
+// }
+
+// ans Number 3
+
+
+
+// let numbersArray = [1, 13, 22, 123, 49]
+// let sum = 0;
+
+// for (let i = 0; i < numbersArray.length; i++) {
+
+//     sum += numbersArray[i];
+
+// }
+
+// console.log(sum)
+
+
+
+// ans Number 4
+
+
+// let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2];
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
+
+// let sum1 = 0
+// let sum2 = 0
+// let total = 0
+
+
+// for (let i = 0; i < arr_1.length; i++) {
+//     sum1 += arr_1[i]
+
+// }
+
+
+// for (let j = 0; j < arr_2.length; j++) {
+
+
+//     sum2 += arr_2[j]
+// }
+
+
+// total = sum1 + sum2;
+// console.log(total)
+
+
+// answer number 5
+
+
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+for(let i = arr.length; i > 0 ; i--){
+    console.log(arr[i]);
+}
